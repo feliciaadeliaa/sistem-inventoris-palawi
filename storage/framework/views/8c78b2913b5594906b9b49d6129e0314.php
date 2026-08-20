c7673bfc98d4aa32a4864b778b9299654934ac23
nzazah <nurza2511@gmail.com>
2026-08-20 13:19:56 +0700

    Implementasi UI

---
+++ storage/framework/views/8c78b2913b5594906b9b49d6129e0314.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH D:\inventoris\sistem-inventoris-palawi\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/cb8ddeab49410b9cdc2c8eb67b9c57ae.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH D:\inventoris\sistem-inventoris-palawi\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>

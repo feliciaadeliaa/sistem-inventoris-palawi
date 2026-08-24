0cb4cd8dd87df6697a9d7d5934866e2ec3d5c6f4
feliciaadeliaa <feliciaa2811@gmail.com>
2026-08-24 09:02:00 +0700

    tambah halaman calendar dengan FullCalendar, perbaiki user dropdown

---
+++ resources/views/calendar/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-xl font-semibold mb-6 text-gray-800 dark:text-white">Calendar</h2>
+    @include('calendar.calendar-area')
+@endsection
